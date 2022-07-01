8ac222574ce4f7cfb9fbd3538b1fbb818be18fdb
NataliaPalacio <npalacioq@unal.edu.co>
2022-07-01 18:52:53 -0500

    Update ingest_data.py

--- src/data/ingest_data.py
+++ src/data/ingest_data.py
@@ -1,8 +1,17 @@
 """
-Módulo de ingestión de datos.
+Módulo ingesta de datos
 -------------------------------------------------------------------------------
+De la ruta https://github.com/jdvelasq/datalabs/blob/master/datasets/precio_bolsa_nacional/ 
+se descargan los archivos de precios de bolsa nacional en formato xls y xlsx a la capa 
+data_lake/landing/
+
+Luego la función ruta_origen verifica que el datalake contenga todas las siguientes 
+carpetas requeridas: .git, .github, .gitignore, .vscode, data_lake, grader.py, Makefile, README.md, src
 
 """
+
+import os
+import wget
 
 
 def ingest_data():
@@ -13,10 +22,30 @@ def ingest_data():
     descarga debe realizarse usando únicamente funciones de Python.
 
     """
-    raise NotImplementedError("Implementar esta función")
+    try:
+        os.chdir("data_lake/landing/")
+        for num in range(1995, 2022):
+            if num in range(2016, 2018):
+                wdir = 'https://github.com/jdvelasq/datalabs/blob/master/datasets/precio_bolsa_nacional/xls/{}.xls?raw=true'.format(
+                    num)
+                wget.download(wdir)
+            else:
+                wdir = 'https://github.com/jdvelasq/datalabs/blob/master/datasets/precio_bolsa_nacional/xls/{}.xlsx?raw=true'.format(
+                    num)
+                wget.download(wdir)
+        os.chdir('../../')
+    except:
+        raise NotImplementedError("Implementar esta función")
+
+
+def test_ruta_origen():
+    assert set(os.listdir()) - set(['.git', '.github', '.gitignore',
+                                    '.vscode', 'data_lake', 'grader.py', 'Makefile', 'README.md', 'src']) == set()
 
 
 if __name__ == "__main__":
+
     import doctest
 
     doctest.testmod()
+    ingest_data()
